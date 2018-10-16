10a6077f970519b32b232653015c1e7fd9069e73
Jan Misar <jan.misar@ackee.cz>
2018-10-16 13:37:11 +0200

    New project.

---
+++ bi-ios-2018/bi-ios-2018/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bi-ios-2018
+//
+//  Created by Jan Misar on 16.10.18.
+//  Copyright © 2018 ČVUT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
