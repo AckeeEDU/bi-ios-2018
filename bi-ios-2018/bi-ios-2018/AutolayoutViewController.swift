854c875dcaada9c99d5d04d136270081eda323b7
Jan Misar <jan.misar@ackee.cz>
2018-10-23 18:05:14 +0200

    UIStackView power 💪

--- bi-ios-2018/bi-ios-2018/AutolayoutViewController.swift
+++ bi-ios-2018/bi-ios-2018/AutolayoutViewController.swift
@@ -18,59 +18,42 @@ class AutolayoutViewController: UIViewController {
         
         // self.label is defined in Main.storyboard
         
-        // Layout definition using basic NSLayoutConstraints
         let greenView = UIView() // 👀 no frame here ☝️
         greenView.backgroundColor = .green
-        view.addSubview(greenView)
-        
-        greenView.translatesAutoresizingMaskIntoConstraints = false
-        let leading = NSLayoutConstraint(item: greenView, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1, constant: 10)
-        let trailing = NSLayoutConstraint(item: greenView, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1, constant: -10)
-        let top = NSLayoutConstraint(item: greenView, attribute: .top, relatedBy: .equal, toItem: label, attribute: .bottom, multiplier: 1, constant: 10)
         let height = NSLayoutConstraint(item: greenView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 100)
-        view.addConstraints([leading, trailing, top])
         greenView.addConstraint(height)
         
-        // other ways to create native constraints ➡️ https://useyourloaf.com/blog/pain-free-constraints-with-layout-anchors/
         
-        // Layout definition using SnapKit
+        
         let redView = UIView()
         redView.backgroundColor = .red
-        view.addSubview(redView)
-        redView.snp.makeConstraints { make in
-            make.leading.equalTo(10)
-            make.top.equalTo(greenView.snp.bottom).offset(20)
-            make.height.equalTo(20)
-        }
         
         let blueView = UIView()
         blueView.backgroundColor = .blue
-        view.addSubview(blueView)
-        blueView.snp.makeConstraints { make in
-            make.leading.equalTo(redView.snp.trailing).offset(20)
-            make.top.height.width.equalTo(redView)
-            make.trailing.equalTo(-10)
+        
+        let redBlueStack = UIStackView(arrangedSubviews: [redView, blueView])
+        redBlueStack.spacing = 10
+        redBlueStack.distribution = .fillEqually
+        redBlueStack.snp.makeConstraints { make in
+            make.height.equalTo(20)
         }
         
-        // Content hugging & compression resistance
         let originalImage = UIImage(named: "demonic")
         let flippedImage = UIImage(cgImage: originalImage!.cgImage!, scale: UIScreen.main.scale, orientation: .upMirrored)
         let leftDemonicView = UIImageView(image: flippedImage)
-        view.addSubview(leftDemonicView)
-        leftDemonicView.snp.makeConstraints { make in
-            make.leading.equalTo(10)
-            make.top.equalTo(blueView.snp.bottom)
-        }
         
         let rightDemonicView = UIImageView(image: #imageLiteral(resourceName: "demonic.png"))
-        view.addSubview(rightDemonicView)
-        rightDemonicView.snp.makeConstraints { make in
-            make.leading.equalTo(leftDemonicView.snp.trailing).offset(15)
-            make.top.equalTo(leftDemonicView)
-            make.trailing.equalTo(-10)
-        }
-        // we want right demonic to win!
         rightDemonicView.setContentHuggingPriority(.required, for: .horizontal)
+        
+        let demonicsStack = UIStackView(arrangedSubviews: [leftDemonicView, rightDemonicView])
+        
+        let mainStack = UIStackView(arrangedSubviews: [label, greenView, redBlueStack, demonicsStack])
+        mainStack.spacing = 15
+        mainStack.axis = .vertical
+        view.addSubview(mainStack)
+        mainStack.snp.makeConstraints { make in
+            make.leading.trailing.top.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
     }
     
     override func viewDidLoad() {
