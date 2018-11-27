dd9c355fd6bceca6b0cd3e57cb26b4708baa37d7
Jan Misar <misar.jan@gmail.com>
2018-11-27 19:28:15 +0100

    Reading user's location.

--- bi-ios-2018/bi-ios-2018/ViewControllers/MapViewController.swift
+++ bi-ios-2018/bi-ios-2018/ViewControllers/MapViewController.swift
@@ -37,6 +37,13 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         if status == .authorizedWhenInUse {
             mapView.showsUserLocation = true
             mapView.userTrackingMode = .follow
+            
+            locationManager.startUpdatingLocation() // we should also stop it somewhere!
         }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last! // we are sure we have at least one location there
+        print(location)
     }
 }
