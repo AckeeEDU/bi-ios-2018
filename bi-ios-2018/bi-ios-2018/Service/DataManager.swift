c611324d1275f1d01291ec7060d847043c841417
Dominik Vesely <dominik.vesely@ackee.cz>
2018-12-04 15:34:29 +0100

    simple download

--- bi-ios-2018/bi-ios-2018/Service/DataManager.swift
+++ bi-ios-2018/bi-ios-2018/Service/DataManager.swift
@@ -13,18 +13,14 @@ class DataManager {
     
     func getReipes( callback: ((Any) -> ())?)  {
         Alamofire.request("https://cookbook.ack.ee/api/v1/recipes").responseJSON { response in
-            print("Request: \(String(describing: response.request))")   // original url request
-            print("Response: \(String(describing: response.response))") // http url response
-            print("Result: \(response.result)")                         // response serialization result
-            
-            if let json = response.result.value {
-                print("JSON: \(json)") // serialized json response
-                callback?(json)
+            if let data = response.data {
+                do {
+                    let jsonDecoder = JSONDecoder()
+                    let recipes = try jsonDecoder.decode([Recipe].self, from: data)
+                    print(recipes)
+                } catch {}
             }
-            
-            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
-                print("Data: \(utf8Text)") // original server data as UTF8 string
-            }
+
         }
     }
     
