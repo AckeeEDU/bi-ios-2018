a19bd05f7d4e9b690ad59fffa8d2ec22d8d9563e
Jan Misar <misar.jan@gmail.com>
2018-12-11 20:41:01 +0100

    We are drawing 🎉

--- bi-ios-2018/bi-ios-2018/ViewControllers/11 CG and Firebase/CanvasView.swift
+++ bi-ios-2018/bi-ios-2018/ViewControllers/11 CG and Firebase/CanvasView.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class CanvasView: UIView {
 
+    var points: [CGPoint] = []
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        backgroundColor = .white
         
         let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panChanged(_:)))
         addGestureRecognizer(gestureRecognizer)
@@ -26,21 +30,37 @@ class CanvasView: UIView {
         case .began:
             break
         case .changed:
-            print(gestureRecognizer.location(in: self))
+            let point = gestureRecognizer.location(in: self)
+            points.append(point)
             break
         case .cancelled, .ended, .failed:
             break
         default:
             break
         }
+        setNeedsDisplay()
     }
     
-    /*
+    
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        super.draw(rect)
+        
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        
+        context.setLineWidth(2)
+        context.setStrokeColor(UIColor.black.cgColor)
+        
+        context.beginPath()
+        
+        if let firstPoint = points.first {
+            context.move(to: firstPoint)
+        }
+        context.addLines(between: points)
+        
+        context.drawPath(using: .stroke)
     }
-    */
+    
 
 }
