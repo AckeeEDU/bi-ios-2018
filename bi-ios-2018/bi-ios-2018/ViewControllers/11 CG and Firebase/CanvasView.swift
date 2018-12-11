06fe354b9fc79483aac6cbe945960ef67ae9b2ee
Jan Misar <misar.jan@gmail.com>
2018-12-11 20:34:40 +0100

    Add gesture recognizer.

--- bi-ios-2018/bi-ios-2018/ViewControllers/11 CG and Firebase/CanvasView.swift
+++ bi-ios-2018/bi-ios-2018/ViewControllers/11 CG and Firebase/CanvasView.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class CanvasView: UIView {
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panChanged(_:)))
+        addGestureRecognizer(gestureRecognizer)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func panChanged(_ gestureRecognizer: UIPanGestureRecognizer) {
+        switch gestureRecognizer.state {
+        case .began:
+            break
+        case .changed:
+            print(gestureRecognizer.location(in: self))
+            break
+        case .cancelled, .ended, .failed:
+            break
+        default:
+            break
+        }
+    }
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
